3162c916a4874c139d9f199044764f7ad8e6e759
Psychokiller1888 <laurentchervet@bluewin.ch>
2020-11-01 18:37:23 +0100

    Starts working

--- ProjectAliceUnittest.py
+++ ProjectAliceUnittest.py
@@ -1,6 +1,9 @@
+import importlib
+import inspect
 from unittest.mock import patch
 
 import mocks.base.model.AliceSkill as AliceSkill
+import mocks.base.SuperManager as SuperManager
 import mocks.dialog.model.DialogSession as DialogSession
 import mocks.util.Decorators as Decorators
 
@@ -13,7 +16,30 @@ class ProjectAliceUnittest:
 			{
 				'core.base.model.AliceSkill': AliceSkill,
 				'core.dialog.model.DialogSession': DialogSession,
-				'core.util.Decorators': Decorators
+				'core.util.Decorators': Decorators,
+				'core.base.SuperManager': SuperManager
 			}
 		)
 		patcher.start()
+
+
+	@property
+	def start(self):
+		# Init the skill we are testing
+		try:
+			caller = inspect.getmodulename(inspect.stack()[1][1])
+			skillName = caller.replace('test_', '')
+			skillImport = importlib.import_module(f'{skillName}')
+			klass = getattr(skillImport, skillName)
+			return klass()
+		except:
+			print("Couldn't import skill to test")
+
+
+	@classmethod
+	def SuperManager(cls): #NOSONAR
+		return SuperManager.SuperManager(**{
+			'talkManager.randomTalk.return_value': 'unittest',
+			'languageManager.activeLanguage.return_value': 'en',
+			'languageManager.supportedLanguages.return_value': ['en']
+		})
---
+++ mocks/base/SuperManager.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from unittest.mock import MagicMock
+
+
+class SuperManager(MagicMock):
+	pass
